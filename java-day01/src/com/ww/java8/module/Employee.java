5af6b8b0706a5ac7d107503a27f6be391d6ed4d3
wangwei <bos@wangwei.com>
2019-01-16 02:30:21 +0800

    重写Employee类的hashCode和equals方法

--- java-day01/src/com/ww/java8/module/Employee.java
+++ java-day01/src/com/ww/java8/module/Employee.java
@@ -61,4 +61,27 @@ public class Employee {
                 ", salary=" + salary +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (age != employee.age) return false;
+        if (Double.compare(employee.salary, salary) != 0) return false;
+        return name != null ? name.equals(employee.name) : employee.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
